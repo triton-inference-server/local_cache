a263c9fee7ebdd4cf42d2116d7a11caa261fcf67
Ryan McCormick <rmccormick@nvidia.com>
2022-11-02 19:52:01 -0700

    Initial commit, add simple cache class and cmake to build

---
+++ src/local_cache.cc
@@ -0,0 +1,40 @@
+#include "local_cache.h"
+
+namespace triton { namespace cache { namespace local {
+
+/* LocalCache Implementation */
+LocalCache::LocalCache(uint64_t size) {
+  // TODO
+  return;
+}
+
+TRITONSERVER_Error* LocalCache::Create(uint64_t cache_size, std::unique_ptr<LocalCache>* cache) {
+  cache->reset(new LocalCache(cache_size));
+  return nullptr;  // success
+}
+
+/* C APIs */
+
+// NOTES: (Remove)
+//   DECLSPEC for Triton-Core defined methods?
+//   ISPEC for Cache Implementation defined methods?
+
+extern "C" {
+
+TRITONSERVER_Error*
+TRITONCACHE_CacheNew(
+  TRITONCACHE_Cache** cache, 
+  TRITONSERVER_Message* cache_config
+)
+{
+  // TODO: Parse cache config for size
+  std::unique_ptr<LocalCache> lcache;
+  constexpr auto cache_size = 4*1024*1024; // 4 MB
+  LocalCache::Create(cache_size, &lcache);
+  *cache = reinterpret_cast<TRITONCACHE_Cache*>(lcache.release());
+  return nullptr;  // success
+}
+
+}  // extern "C"
+
+}}}  // triton::cache::local
---
+++ src/local_cache.h
@@ -0,0 +1,35 @@
+#include "triton/core/tritoncache.h"
+#include "triton/core/tritonserver.h"
+#include <memory>
+#include <vector>
+
+namespace triton { namespace cache { namespace local {
+
+struct CacheEntry {
+  void* data;
+  size_t size;
+};
+
+class LocalCache {
+  public:
+    // Create the cache object
+    static TRITONSERVER_Error* Create(uint64_t cache_size, std::unique_ptr<LocalCache>* cache);
+
+    // Lookup key in cache and return the data associated with it
+    // Return TRITONSERVER_Error* object indicating success or failure.
+    TRITONSERVER_Error* Lookup(std::string key, std::vector<CacheEntry>* entries);
+
+    // Insert entries into cache, evict entries to make space if necessary
+    // Return TRITONSERVER_Error* object indicating success or failure.
+    TRITONSERVER_Error* Insert(std::string key, const std::vector<CacheEntry>& entries);
+
+    // Evict entries from cache based on policy. 
+    // Return TRITONSERVER_Error* object indicating success or failure.
+    TRITONSERVER_Error* Evict();
+
+  private:
+    // TODO
+    LocalCache(uint64_t size);
+};
+
+}}}  // triton::cache::local
